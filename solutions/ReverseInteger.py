933d61f62df9adbff7e56106444dd51b0ec3be5c
matias <matiaseidis@gmail.com>
2015-04-02 19:09:17 -0300

    reverse int improved

--- solutions/ReverseInteger.py
+++ solutions/ReverseInteger.py
@@ -3,7 +3,24 @@
 # Example2: x = -123, return -321
 
 # @return an integer
-def reverse(self, x):
+def reverse(self, n):
+	positive = n>=0
+	total_digits = len(str(n))-1
+	if not positive:
+		total_digits -= 1
+	n = abs(n)
+	result = 0
+	
+	for i in range(total_digits, -1, -1):
+		current = n / 10**i
+		n -= current * 10**i
+		result += (current * 10**(total_digits-i))
+		
+	return result if positive else -result
+	
+	
+
+def reverse2(self, x):
 	x_str = str(x)
 	magnitude = len(x_str)
 	start = 1 if x < 0 else 0
@@ -24,5 +41,5 @@ def test(exp, result):
 	print("ok" if exp == result else "no - exp: {0} result: {1}".format(exp, result))
 	
 
-test(123, reverse(None, 321))
+#test(123, reverse(None, 321))
 test(-123, reverse(None, -321))
